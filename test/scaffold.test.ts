07fc8d39817be2b1d85887459b747eeeb67930ca
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2026-07-30 15:47:30 +0900

    v2.0.2

--- test/scaffold.test.ts
+++ test/scaffold.test.ts
@@ -23,7 +23,7 @@ describe('generatePackageJson', () => {
     const content = JSON.parse(generatePackageJson('my-bot'));
     expect(content.name).toBe('my-bot');
     expect(content.scripts.dev).toBe('disbord dev');
-    expect(content.dependencies.disbord).toBe('^2.0.1');
+    expect(content.dependencies.disbord).toBe('^2.0.2');
   });
 
   test('dev以外のnpm scripts(build/once/fmt/lint/gen:event/gen:once/enable/disable/env/encrypt/decrypt/help)も含む', () => {
